a099f0be70562516fb90e1543906686272d017d7
Asanchez <asanchez@lsv-tech.com>
2022-05-24 20:08:56 -0500

    clases homeview y main done

---
+++ lib/view/counter.dart
@@ -0,0 +1,47 @@
+// import 'package:flutter/material.dart';
+
+// class Contador extends StatelessWidget{
+//   const Contador({Key? key}) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context){
+//     return Scaffold(
+//       appBar: AppBar(
+//         elevation: 8.0,
+//         backgroundColor: const Color.fromARGB(255,241,222,222),
+//         title: const Text('Home'),
+//       ),
+
+//       body: Center(
+//         child: Column(
+          
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: <Widget>[
+//             const Text("Number push",
+//                 style: TextStyle(fontSize:24),
+//             ),
+//             Text(
+//               '01',
+//               style: siseText,
+//             ),
+//           ], 
+//           ),
+//         )
+//         floatingActionButton: FloatingActionButton(
+//           child: const Icon(Icons.access_alarm),
+//           onPressed: (){
+//             count++;
+//             showMessage();
+
+//         }));
+  
+  
+//   void showMessage(){
+//     print("Hi Again")
+//   }
+
+
+//   }
+// }
+
+  
